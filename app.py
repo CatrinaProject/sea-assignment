25854876c5a90f58771e0b99c71ca82e696d5b75
Catrina Chau <pccatrin@amazon.com>
2024-02-19 10:56:23 +0000

    Add logging module to app.py

--- app.py
+++ app.py
@@ -3,7 +3,8 @@
 
 import re
 from datetime import timedelta
-from flask import Flask, redirect, render_template, request, session, flash, url_for
+import logging
+from flask import Flask, redirect, render_template, request, session, flash
 from helpers import hash_password, register_user_to_db, check_user, is_admin
 from televisions import televisions, add_television_record, edit_television, update_television_record, delete_television
 from tests import tests, add_test_record, edit_tests, update_test_record, delete_test
@@ -15,6 +16,16 @@ app.permanent_session_lifetime = timedelta(minutes=60)
 app.config['SESSION_COOKIE_SECURE'] = True
 app.config['SESSION_COOKIE_HTTPONLY'] = True
 
+def create_logger():
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    file_handler = logging.FileHandler('app.log')
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+    return logger
+
+logger = create_logger()
 
 
 @app.before_request  # Before a each request, check whether the page is a /admin route
@@ -27,6 +38,7 @@ def check_admin_route():
 
 @app.route("/")  # Main route: renders the login page
 def index():
+    logger.info('Processing login page')
     return render_template('login.html')  # Renders the login page
 
 
@@ -66,8 +78,10 @@ def register():
         hashed_password = hash_password(password)
 
         register_user_to_db(username, hashed_password)  # Call register user to database with the username and hashed password
+        logger.info("Registered a new user to the database")
         return redirect("/")  # redirect to the login page
     else:
+        logger.error('Unable tp register new user, redirecting back to Registration')
         return render_template('register.html')  # render the registration page
 
 
@@ -78,8 +92,10 @@ def login():
         password = request.form['password']
         if check_user(username, password):  # Creates a new session for the user, then redirects them to the home page
             session['username'] = username  # Add username to the session
+            logger.info('Login succeeded, processing home page')
             return redirect("/home")  # Go to the home page
         else:
+            logger.error('Could not login user: %s, as username or password was invalid, redirecting to login failed page', username)
             return render_template('login-failed.html')  # Go to login-failed page when their user doesn't exist
     else:
         return redirect("/")
@@ -90,67 +106,80 @@ def home():
     if 'username' in session:
         return render_template('home.html', username=session['username'])
     else:
+        logger.error('User not in session, redirecting to login failed page')
         return render_template('login-failed.html')
 
 
 @app.route('/televisions', methods=['GET'])  # Route to display a list of televisions
 def televisions_route():
+    logger.info('Processing televisions page')
     return televisions()
 
 
 @app.route('/televisions/add', methods=['POST'])  # Route to add a new television record
 def televisions_add_roue():
+    logger.info('Adding a new television record')
     return add_television_record()
 
 
 @app.route('/televisions/edit', methods=['GET'])  # Route to load the 'edit a television record' page
 def televisions_edit_route():
+    logger.info('Editing a television record')
     return edit_television()
 
 
 @app.route('/televisions/edit/submit', methods=['POST'])  # Route to edit a television record
 def televisions_edit_submit_route():
+    logger.info('Updating a television record')
     return update_television_record()
 
 
 @app.route('/admin/televisions/delete', methods=['GET'])  # Route to delete a television record
 def television_delete_route():
+    logger.info('Deleting a television record')
     return delete_television()
 
 
 @app.route('/tests', methods=['GET'])  # Route to display a list of tests
 def tests_route():
+    logger.info('Processing tests page')
     return tests()
 
 
 @app.route('/tests/add', methods=['POST'])  # Route to add a new test record
 def tests_add_route():
+    logger.info('Adding a new test record')
     return add_test_record()
 
 
 @app.route('/tests/edit', methods=['GET'])  # Route to load the 'edit a test record' page
 def tests_edit_route():
+    logger.info('Editing a test record')
     return edit_tests()
 
 
 @app.route('/tests/edit/submit', methods=['POST'])  # Route to edit a test record
 def tests_edit_submit_route():
+    logger.info('Updating a test record')
     return update_test_record()
 
 
 @app.route('/admin/tests/delete', methods=['GET'])  # Route to delete a test record
 def test_delete_route():
+    logger.info('Deleting a test record')
     return delete_test()
 
 
 @app.route('/admin/dashboard', methods=["GET", "POST"])  # Admin dashboard route: displays pending users and admin users
 def admin_approval_dashboard_route():
+    logger.info('Processing admin dashboard')
     return admin_dashboard()
 
 
 @app.route('/logout')  # Logout route: logs the user out by clearing session
 def logout():
     session.clear()
+    logger.info('Session is cleared, logging the user out')
     return redirect("/")  # Redirect to the login page
 
 
